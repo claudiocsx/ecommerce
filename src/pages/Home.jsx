13d9f2963fe15f449314d6e7299fe46011234405
unknown <claudiosasuke@gmail.com>
2021-09-17 20:46:37 -0300

    componet Newsletter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Categories from '../components/Categories'
 import NavBar from '../components/NavBar'
 import Slider from '../components/Slider'
 import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
+
 
 function Home() {
     return (
@@ -13,6 +15,7 @@ function Home() {
             <Slider/>
             <Categories/>
             <Products/>
+            <Newsletter/>
         </div>
     )
 }
