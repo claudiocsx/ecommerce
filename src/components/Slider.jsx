dc70bc3a3f245da53ac604d9ddf27d23adfb811e
unknown <claudiosasuke@gmail.com>
2021-09-17 19:50:03 -0300

    produtos

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -89,7 +89,7 @@ const Slider = () => {
             <Wrapper slideIndex={slideIndex}>
                 {sliderItems.map(item=>(
 
-                    <Slide bg={item.bg}>
+                    <Slide bg={item.bg} key={item.id}>
                     <ImageContainer>
                         <Image src={item.img} />
                     </ImageContainer>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Announcement from '../components/Announcement'
 import Categories from '../components/Categories'
 import NavBar from '../components/NavBar'
 import Slider from '../components/Slider'
+import Products from '../components/Products'
 
 function Home() {
     return (
@@ -11,6 +12,7 @@ function Home() {
             <NavBar/>
             <Slider/>
             <Categories/>
+            <Products/>
         </div>
     )
 }
