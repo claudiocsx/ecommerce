dcfd159b33c170fb9817140155967ad82eae6f23
unknown <claudiosasuke@gmail.com>
2021-09-17 15:56:52 -0300

    slide finalizado

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -31,7 +31,8 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
     display: flex; 
     height: 100%;
-    transform: translateX(0vw);
+    transition: all 1.5s ease;
+    transform: translateX(${props=>props.slideIndex * -100}vw);
 `
 const Slide = styled.div`
     width: 100vw;
@@ -70,10 +71,14 @@ cursor: pointer;
 `
 
 const Slider = () => {
-    const [slideIndex, setSlideInex]= useState(0)
+    const [slideIndex, setSlideIndex]= useState(0)
     
     const handleClick = (direction) => {
-
+        if(direction==="left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2 )
+        }else{
+            setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0)
+        }
     }
 
     return (
@@ -81,7 +86,7 @@ const Slider = () => {
             <Arrow direction="left" onClick={()=>handleClick("left")}>
                 <ArrowLeftOutlined/>
             </Arrow>
-            <Wrapper>
+            <Wrapper slideIndex={slideIndex}>
                 {sliderItems.map(item=>(
 
                     <Slide bg={item.bg}>
